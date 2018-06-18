5bd34305194b6b369145a06bb2c02c78d39c6dc6
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-06-18 20:21:16 +0100

    Erl ffi updates from merge changes

--- src/Global.erl
+++ src/Global.erl
@@ -1,13 +1,7 @@
 -module(global@foreign).
--export([isFinite/1, readInt/2, readFloat/1, decodeURI/1, encodeURI/1, decodeURIComponent/1, encodeURIComponent/1]).
+-export([isFinite/1, readInt/2, readFloat/1]).
 
 isFinite(_) -> true.
 
 readInt(Radix, N) -> binary_to_integer(N, Radix).
 readFloat(N) -> binary_to_float(N).
-
-decodeURI(X) -> http_uri:decode(X).
-encodeURI(X) -> http_uri:encode(X).
-
-decodeURIComponent(X) -> http_uri:decode(X).
-encodeURIComponent(X) -> http_uri:encode(X).
--- src/Global/Unsafe.erl
+++ src/Global/Unsafe.erl
@@ -1,7 +1,12 @@
 -module (global_unsafe@foreign).
--export([unsafeStringify/1, unsafeToFixed/2, unsafeToExponential/2, unsafeToPrecision/2]).
+-export([unsafeStringify/1, unsafeToFixed/2, unsafeToExponential/2, unsafeToPrecision/2, unsafeDecodeURI/1, unsafeEncodeURI/1, unsafeDecodeURIComponent/1, unsafeEncodeURIComponent/1]).
 
-unsafeStringify(X) -> error("very unsafe").
+unsafeStringify(_) -> error("very unsafe").
 unsafeToFixed(Digits, N) -> float_to_binary(N, {decimals, Digits}).
 unsafeToExponential(Digits, N) -> float_to_binary(N, {scientific, Digits}).
 unsafeToPrecision(Digits, N) -> unsafeToPrecision(Digits, N). 
+
+unsafeDecodeURI(X) -> http_uri:decode(X).
+unsafeEncodeURI(X) -> http_uri:encode(X).
+unsafeDecodeURIComponent(X) -> http_uri:decode(X).
+unsafeEncodeURIComponent(X) -> http_uri:encode(X).
